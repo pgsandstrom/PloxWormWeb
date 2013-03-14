2ae59ab129170be0936cb5a168e8ac18b3a6b279
Per Sandstrom <per.sandstrom@sogeti.se>
2013-03-14 13:58:02 +0100

    initial

---
+++ main/src/se/persandstrom/ploxworm/web/Test.java
@@ -0,0 +1,14 @@
+package se.persandstrom.ploxworm.web;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named("test")
+@ApplicationScoped
+public class Test implements Serializable {
+
+    public String getTest() {
+        return "test";
+    }
+}
