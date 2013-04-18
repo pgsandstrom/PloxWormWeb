aaf46c61e9e5dd6050466a5ba22b3977eeb3567b
Per Sandstrom <per.sandstrom@sogeti.se>
2013-04-18 13:45:27 +0200

    new line intersection algorithm

--- main/src/se/persandstrom/ploxworm/core/Line.java
+++ main/src/se/persandstrom/ploxworm/core/Line.java
@@ -2,27 +2,16 @@ package se.persandstrom.ploxworm.core;
 
 public class Line {
 
-    //start:
     public final double xStart;
     public final double yStart;
 
-    //stop:
     public final double xStop;
     public final double yStop;
-
-    //func:
-    public final double a;
-    public final double c;
 
     public Line(double xStart, double yStart, double xStop, double yStop) {
         this.xStart = xStart;
         this.yStart = yStart;
         this.xStop = xStop;
         this.yStop = yStop;
-
-        a = (yStop - yStart) / (xStop - xStart);
-        c = yStart - a * xStart;
     }
-
-   
 }
